fffb0d556f4ddeb31f466fe7a5f61fe9ddd22ba7
mykahmarks <78816351+mykahmarks@users.noreply.github.com>
2022-05-06 12:37:04 -0700

    Update Method.java

--- src/com/marksware/simplescheduler/presentation/Method.java
+++ src/com/marksware/simplescheduler/presentation/Method.java
@@ -1,1 +1,24 @@
 
+package com.marksware.simplescheduler.presentation;
+
+import com.marksware.simplescheduler.models.SimpleEvent;
+
+import java.io.*;
+import java.security.GeneralSecurityException;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SchedulingUtil {
+
+    public enum ScheduledIdentifiers { _SimpleSchedule_,
+        _SimpleSchedule_Generated,
+        _SimpleSchedule_OriginalStartDateTime, _SimpleSchedule_OriginalEndDateTime,
+        _SimpleSchedule_UpdatedStartDateTime, _SimpleSchedule_UpdatedEndDateTime
+    };
+
+    public enum SchedulingCommands { _SimpleSchedule_,
+        _SimpleSchedule_SplitInHalfToNextDay
+    };
